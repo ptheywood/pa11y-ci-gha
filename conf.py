f23a8665ff94b97f5857fdbdc20a76e240024407
Peter Heywood <peethwd@gmail.com>
2022-03-24 12:09:19 +0000

    Add some custom css to set bad contrast

--- conf.py
+++ conf.py
@@ -50,3 +50,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Add custom css to override theme defaults
+html_css_files = [
+    'css/custom.css',
+]
